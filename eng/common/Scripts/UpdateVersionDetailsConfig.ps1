172c38478fb0410626ab37298c43522882fd5344
Will Thant <kythant@microsoft.com>
2021-11-30 10:31:07 -0800

    fix

--- eng/common/Scripts/UpdateVersionDetailsConfig.ps1
+++ eng/common/Scripts/UpdateVersionDetailsConfig.ps1
@@ -1,14 +1,29 @@
 [CmdLetBinding()]
 Param(
-    [string]$versionDetailsPath,
     [string]$dependencyName,
     [string]$dependencyVersion
 )
 
-[xml]$VersionDetails = Get-Content -Encoding utf8 -Path $VersionDetailsPath
+# Get the root of the repo.
+$scriptFullPath =  (split-path -parent $MyInvocation.MyCommand.Definition)
+$rootPath = (split-path -parent $scriptFullPath)
 
-$dependency = $buildConfig.Dependencies.ProductDependencies.Dependency | where {$_.Name -eq "$dependencyName"}
-$dependency.Version = $dependencyVersion
+Function CheckFile($filename)
+{
+    if(-not (Test-Path $filename))
+    {
+        write-host "File not found: $filename"
+        exit 1
+    }
+}
 
-$VersionDetails.Save($VersionDetailsPath)
-Write-Host "Saved versions back to $VersionDetailsPath"
+$configFilename = "$rootPath\eng\Version.Details.xml"
+CheckFile $configFilename
+
+# Load the build.config, update the requested version entry, then write it back out
+$xmldoc = [System.Xml.XmlDocument](Get-Content $configFilename)
+$node = $xmldoc.Dependencies.ProductDependencies.Dependency | ?{$_.Name -eq $dependencyName}
+$node.Version = $dependencyVersion
+$xmldoc.Save($configFilename)
+
+write-host "Updated $configFilename"
